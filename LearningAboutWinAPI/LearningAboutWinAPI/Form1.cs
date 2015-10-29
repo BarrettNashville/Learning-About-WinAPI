ffadae1ae5cfb8ee36e0d5945ea53c903caa6e5a
unknown <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-28 20:43:15 -0500

    Append text from C# form to an open Notepad window

--- LearningAboutWinAPI/LearningAboutWinAPI/Form1.cs
+++ LearningAboutWinAPI/LearningAboutWinAPI/Form1.cs
@@ -22,22 +22,42 @@ namespace LearningAboutWinAPI
          * and
          * http://stackoverflow.com/questions/6604070/write-text-to-notepad-with-c-win32 */
 
-        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
-        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        extern static IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, [In] string lpClassName, [In] string lpWindowName);
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        extern static int SendMessageGetTextLength(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
         [DllImport("User32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
 
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, int lParam);
+
+        const int WM_GETTEXTLENGTH = 0x000E;
+
+        const int EM_SETSEL = 0x00B1;
+
+        const int EM_REPLACESEL = 0x00C2;
+
+        public void testAppendText(string text)
+        {
+            Process[] notepads = Process.GetProcessesByName("notepad");
+            if (notepads.Length == 0) return;
+            if (notepads[0] != null)
+            {
+                IntPtr editBox = FindWindowEx(notepads[0].MainWindowHandle, new IntPtr(0), "Edit", null);
+                int length = SendMessageGetTextLength(editBox, WM_GETTEXTLENGTH, IntPtr.Zero, IntPtr.Zero);
+                SendMessage(editBox, EM_SETSEL, length, length);
+                SendMessage(editBox, EM_REPLACESEL, 1, text);
+            }
+
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                Process[] notepads = Process.GetProcessesByName("notepad");
-                if (notepads.Length == 0) return;
-                if (notepads[0] != null)
-                {
-                    IntPtr child = FindWindowEx(notepads[0].MainWindowHandle, new IntPtr(0), "Edit", null);
-                    SendMessage(child, 0x000C, 0, textBox1.Text);
-                }
+                testAppendText(textBox1.Text);
             }
             catch (Exception er)
             {
