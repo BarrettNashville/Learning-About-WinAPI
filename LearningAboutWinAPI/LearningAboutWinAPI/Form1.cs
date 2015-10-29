6793bb3bec074eeee2bdf9e1791684d3b1df6c0f
unknown <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-28 20:27:57 -0500

    Simple Program to Get CPU Info

--- LearningAboutWinAPI/LearningAboutWinAPI/Form1.cs
+++ LearningAboutWinAPI/LearningAboutWinAPI/Form1.cs
@@ -18,12 +18,42 @@ namespace LearningAboutWinAPI
             InitializeComponent();
         }
 
-        [DllImport("user32.dll")]
-        public static extern int MessageBox(int h, string m, string c, int type);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEM_INFO
+        {
+            public uint dwOemId;
+            public uint dwPageSize;
+            public uint lpMinimumApplicationAddress;
+            public uint lpMaximumApplicationAddress;
+            public uint dwActiveProcessorMask;
+            public uint dwNumberOfProcessors;
+            public uint dwProcessorType;
+            public uint dwAllocationGranularity;
+            public uint dwProcessorLevel;
+            public uint dwProcessorRevision;
+        }
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern void GetSystemInfo(ref SYSTEM_INFO lpSystemInfo);
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox(0, "API Message Box", "API Demo", 0);
+            try
+            {
+                SYSTEM_INFO pSI = new SYSTEM_INFO();
+                GetSystemInfo(ref pSI);
+
+                listBox1.Items.Insert(0, "Number of Processors: " + pSI.dwNumberOfProcessors.ToString());
+                listBox1.Items.Insert(1, "Processor Type: " + pSI.dwProcessorType.ToString());
+                listBox1.Items.Insert(2, "Page Size: " + pSI.dwPageSize);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+
         }
     }
 }
